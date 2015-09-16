2cd9e694c9ace3212f3065926b4a45653c8682f9
Sander Tuin <sander@hackerone.com>
2015-09-16 13:03:21 +0200

    Adding decimal constraint with precision and scale

--- lib/stronger_parameters/constraints/decimal_constraint.rb
+++ lib/stronger_parameters/constraints/decimal_constraint.rb
@@ -11,17 +11,18 @@ module StrongerParameters
 
     def value(v)
       if scale > 0
-        if v.is_a?(Float)
+        if v.is_a?(Float) && (v.abs.to_s.size - 1) <= precision
           return value(v.to_s)
-        elsif v.is_a?(String) && v =~ /\A-?\d+\.?\d{#{scale}}\Z/
+        elsif v.is_a?(String) && v =~ /\A-?\d{1,#{precision - scale}}\.?\d{#{scale}}\Z/
           return v.to_d
         end
-      else
-        integer = IntegerConstraint.new
-        return integer.value(v)
+      elsif (v.is_a?(Fixnum) || v.is_a?(Bignum)) && (v.abs.to_s.size <= precision)
+        return v.to_d
+      elsif v.is_a?(String) && v =~ /\A-?\d{1,#{precision - scale}}\Z/
+        return v.to_d
       end
 
-      StrongerParameters::InvalidValue.new(v, 'invalid decimal')
+      StrongerParameters::InvalidValue.new(v, 'must be a decimal')
     end
   end
 end
--- test/decimal_constraint_test.rb
+++ test/decimal_constraint_test.rb
@@ -1,7 +1,7 @@
 require_relative 'test_helper'
 
 describe 'decimal parameter constraints' do
-  describe 'with 2 scale' do
+  describe 'with 2 scale and 4 precision' do
     subject { ActionController::Parameters.decimal(4, 2) }
 
     permits '1.23', as: 1.23
@@ -32,20 +32,35 @@ describe 'decimal parameter constraints' do
     rejects '-123.45', as: 123.45
     rejects 123.45
     rejects -123.45
-
   end
 
-  # describe 'with 0 scale' do
-  #   subject { ActionController::Parameters.decimal(5, 0) }
+  describe 'with 0 scale and 5 precision' do
+    subject { ActionController::Parameters.decimal(5, 0) }
 
-  #   permits 1
-  #   permits '1', as: 1
-  #   permits -1
-  #   permits '-1', as: -1
+    permits 1
+    permits '1', as: 1
+    permits -1
+    permits '-1', as: -1
 
-  #   rejects '1.23', as: 1.23
-  #   rejects '-1.23', as: -1.23
-  #   rejects 1.23
-  #   rejects -1.23
-  # end
+    permits 12345
+    permits '12345', as: 12345
+    permits -12345
+    permits '-12345', as: -12345
+
+    rejects 'abc'
+    rejects '1.2.3'
+    rejects '.3'
+    rejects '3.'
+    rejects true
+
+    rejects '1.23', as: 1.23
+    rejects '-1.23', as: -1.23
+    rejects 1.23
+    rejects -1.23
+
+    rejects 123456
+    rejects '123456', as: 123456
+    rejects -123456
+    rejects '-123456', as: -123456
+  end
 end
